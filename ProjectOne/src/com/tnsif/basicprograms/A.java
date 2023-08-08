e939eace3fb8d7da492e8db06688f937e3e560d8
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2023-08-08 11:54:12 +0530

    First commit

--- ProjectOne/src/com/tnsif/basicprograms/A.java
+++
@@ -1,5 +0,0 @@
-package com.tnsif.basicprograms;
-
-public class A {
-
-}
---
+++ ProjectOne/src/com/tnsif/basicprograms/CharDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.basicprograms;
+//program to demonstrate char data type
+/* */
+public class CharDemo {
+	public static void main(String[] args) {
+		// assigning single character literal
+		char ch='a';
+		System.out.println(ch);
+		
+		// assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		
+		// ASCI code representation
+		int a = 'B';
+		System.out.println(a);	
+	}
+}
